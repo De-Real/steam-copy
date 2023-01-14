6917d42962d139099cf9fa501ba0ea9d8f3e3797
Real <vinarchuk.a.v@.gmail.com>
2023-01-14 14:19:10 +0200

    mainItems refactored

---
+++ src/api/steam-search-options.ts
@@ -0,0 +1,7 @@
+export const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '7c5ef87773msh1ba3465dbccadb6p1e9a59jsnbe646dae5f80',
+		'X-RapidAPI-Host': 'steam2.p.rapidapi.com'
+	}
+};
--- src/components/Main/MainItems.tsx
+++ src/components/Main/MainItems.tsx
@@ -5,64 +5,57 @@ import Pagination from '../UI/Pagination';
 import MainItem from './MainItem/MainItem'
 
 import { filterArray } from '../../util/filterArray';
+import { scrollTop } from '../../util/scrollTop';
+import { FetchingData } from '../../types/fetchingDataInterface';
+import loadingGif from '../../assets/loading.gif'
 
 import './MainItems.scss'
-import { FetchingData } from '../../types/fetchingDataInterface';
-
-const scrollTop = () => {
-	window.scrollTo({
-		top: 0,
-		left: 0,
-		behavior: 'smooth',
-	})
-}
+import { options } from '../../api/steam-search-options';
 
 const MainItems = () => {
 
-	// const DUMMY_ARRAY = useAppSelector((state) => state.products.products);
+	const postsPerPage = 25;
+
 	const [initialPosts, setInitialPosts] = useState<FetchingData[]>([]);
 	const [blogPosts, setBlogPosts] = useState<FetchingData[]>([]);
 	const [currentPage, setCurrentPage] = useState(1);
-	const [postsPerPage] = useState(25);
+
+	const [isLoading, setIsLoading] = useState(true);
+	const [error, setError] = useState('');
 
 	const { searchingValue, orderFilterValue, droppableFilterValue } = useAppSelector((state) => state.filterParams)
 
+	const navigate = useNavigate();
+	const { pageNumber } = useParams()
+
 	useEffect(() => {
-
-		const options = {
-			method: 'GET',
-			headers: {
-				'X-RapidAPI-Key': '7c5ef87773msh1ba3465dbccadb6p1e9a59jsnbe646dae5f80',
-				'X-RapidAPI-Host': 'steam2.p.rapidapi.com'
-			}
-		};
-
 		const fetchData = async () => {
+			setError('');
+			setIsLoading(true);
 			const response = await fetch(`https://steam2.p.rapidapi.com/search/${searchingValue}/page/${currentPage}`, options);
 
 			if (!response.ok) {
-				throw new Error('Error!!!');
+
+				setIsLoading(false);
+				setError(`Can't fetch data. Reason: ${response.statusText}`);
+				return;
 			}
 
-			const results = await response.json(); 
-			console.log(results);
+			const results = await response.json();
+
 			setInitialPosts(results);
+			if (results.length === 0) {
+				setError('No info found!');
+			}
+			setIsLoading(false);
 		}
 		fetchData();
 	}, [currentPage, searchingValue])
 
 
 	useEffect(() => {
-		let temp = [...initialPosts];
-		console.log(temp)
-		temp = filterArray(temp, orderFilterValue, droppableFilterValue)
-		console.log(temp)
-		setBlogPosts(temp);
+		setBlogPosts(filterArray([...initialPosts], orderFilterValue, droppableFilterValue));
 	}, [droppableFilterValue, orderFilterValue, initialPosts])
-
-	const navigate = useNavigate();
-
-	const { pageNumber } = useParams()
 
 	useEffect(() => {
 		if (pageNumber) {
@@ -82,18 +75,14 @@ const MainItems = () => {
 
 	return (
 		<section className='main-items'>
-
-
 			<div>
-				{Boolean(currentPosts.length) &&
-					<ul className='main-items__items'>
-						{currentPosts.map((item) => {
-							return <MainItem key={item.appId} product={item} />
-						})}
-					</ul>
-				}
-				{!currentPosts.length && <p> No data found!</p>}
-
+				{isLoading && <img src={loadingGif} alt='Loading gif' />}
+				{error.length > 0 && <p>{error}</p>}
+				{!isLoading && error.length === 0 && <ul className='main-items__items'>
+					{currentPosts.map((item) => {
+						return <MainItem key={item.appId} product={item} />
+					})}
+				</ul>}
 			</div>
 
 
